221dc5b4effb74ae594be841c2a8a0481d6cbfdd
Einsteinirinium <david_steiner1@sluz.ch>
2026-09-01 12:36:53 +0200

    Speichern und hochladen

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿Console.WriteLine("Hello Git! Ich lerne grade Versinskontrolle.");
 Console.WriteLine("Please help me, I need it.");
 Console.WriteLine("This line only exist to test GitHub");
+Console.WriteLine("Diese Zeile will ich gar nicht behalten");
